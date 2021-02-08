d204777bc80fa3e83a89b8840d81b216866ec2f8
Sam <samsalfi@yahoo.ca>
2021-02-07 21:36:41 -0400

    update

--- Assets/Scripts/DimensionSwapping.cs
+++ Assets/Scripts/DimensionSwapping.cs
@@ -12,6 +12,7 @@ public class DimensionSwapping : MonoBehaviour
     public Camera dimension2Camera;
 
     public Material dimensionPreviewMaterial;
+    public Material firstDimensionPreviewMaterial;
     public Material screenDimensionPreviewMaterial;
 
 
@@ -26,6 +27,8 @@ public class DimensionSwapping : MonoBehaviour
     ColorAdjustments colorAdjustment;
     LensDistortion lensDistortion;
 
+    bool previewingDimension = false;
+
     void Start()
     {
         DimensionFilter[] objects = Resources.FindObjectsOfTypeAll(typeof(DimensionFilter)) as DimensionFilter[];
@@ -34,6 +37,8 @@ public class DimensionSwapping : MonoBehaviour
         dimensionPreviewVolume.profile.TryGet(out chromaticAbberation);
         dimensionPreviewVolume.profile.TryGet(out colorAdjustment);
         dimensionPreviewVolume.profile.TryGet(out lensDistortion);
+        screenDimensionPreviewMaterial.SetFloat("InnerColorCircleSize", 0);
+        screenDimensionPreviewMaterial.SetFloat("SaturatedCircleSize", 0);
     }
 
     void UpdateAllMaterials()
@@ -42,48 +47,100 @@ public class DimensionSwapping : MonoBehaviour
         DimensionFilter[] dimensions = Resources.FindObjectsOfTypeAll(typeof(DimensionFilter)) as DimensionFilter[];
         foreach (DimensionFilter dim in dimensions)
         {
-            if (dim.dimension != currentDimension)
+            if (previewingDimension)
             {
-                dim.GetComponent<Renderer>().material = dimensionPreviewMaterial;
-            }
-            else
+
+                if (dim.dimension != currentDimension)
+                {
+                    dim.GetComponent<Renderer>().material = dimensionPreviewMaterial;
+                }
+                else
+                {
+                    dim.GetComponent<Renderer>().material = firstDimensionPreviewMaterial;
+                }
+            } else
             {
-                dim.GetComponent<Renderer>().material = dim.startingMaterial;
+                if (dim.dimension == currentDimension)
+                {
+                    dim.GetComponent<Renderer>().material = dim.startingMaterial;
+                } else
+                {
+                    if (dim.dimension != currentDimension)
+                    {
+                        dim.GetComponent<Renderer>().material = dimensionPreviewMaterial;
+                    }
+                    else
+                    {
+                        dim.GetComponent<Renderer>().material = firstDimensionPreviewMaterial;
+                    }
+                }
             }
+        }
+    }
+
+    float v = 0.0f;
+    float v2 = 0.0f;
+
+    bool UpdateSaturationCircles()
+    {
+        float currentSaturatedCircleSize = screenDimensionPreviewMaterial.GetFloat("SaturatedCircleSize");
+        float saturatedCircleTargetSize = -0.63f;
+
+        float currentColorCircleSize = screenDimensionPreviewMaterial.GetFloat("InnerColorCircleSize");
+        float colorCircleTargetSize = 0f;
+
+        //screenDimensionPreviewMaterial.SetFloat("SaturatedCircleSize", Mathf.SmoothStep(-0.63f, 0.6f, perc));
+
+        if (Input.GetButton("PreviewDimension"))
+        {
+            screenDimensionPreviewMaterial.SetFloat("DistortionCircle", 0.6f);
+            saturatedCircleTargetSize = 0.6f;
+            if (currentDimension == DimensionFilter.Dimension.Two)
+            {
+                colorCircleTargetSize = 0.6f;
+            }
+        } else
+        {
+            screenDimensionPreviewMaterial.SetFloat("DistortionCircle", -0.2f);
+        }
+
+        if (currentDimension == DimensionFilter.Dimension.Two)
+        {
+            saturatedCircleTargetSize = 2f;
+
+        }
+        float saturatedCircleSize = Mathf.SmoothDamp(currentSaturatedCircleSize, saturatedCircleTargetSize, ref v, 0.05f, 10f, Time.deltaTime / Time.timeScale);
+        float colorCircleSize = Mathf.SmoothDamp(currentColorCircleSize, colorCircleTargetSize, ref v2, 0.03f, 10f, Time.deltaTime / Time.timeScale);
+        screenDimensionPreviewMaterial.SetFloat("SaturatedCircleSize", saturatedCircleSize);
+        screenDimensionPreviewMaterial.SetFloat("InnerColorCircleSize", colorCircleSize);
+        if (saturatedCircleSize == currentSaturatedCircleSize && colorCircleSize == currentColorCircleSize)
+        {
+            return false;
+        } else
+        {
+            return true;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        previewingDimension = Input.GetButton("PreviewDimension");
+
         currentLerpTime += lerpSpeed * Time.deltaTime / Time.timeScale;
         currentLerpTime = Mathf.Clamp(currentLerpTime, 0, lerpTime);
         float perc = currentLerpTime / lerpTime;
 
-        //dimensionPreviewMaterial.SetFloat("_Fade", Mathf.Lerp(0f, 0.8f, perc));
         dimensionPreviewMaterial.SetFloat("WorldCircleSize", Mathf.Lerp(-0.07f, 0.57f, perc));
-        screenDimensionPreviewMaterial.SetFloat("CircleSize", Mathf.Lerp(-0.07f, 0.6f, perc));
+        firstDimensionPreviewMaterial.SetFloat("WorldCircleSize", Mathf.Lerp(-0.07f, 0.57f, perc));
+
+
         vignette.intensity.value  = Mathf.Lerp(0f, 0.4f, perc);
         chromaticAbberation.intensity.value = Mathf.Lerp(0f, 0.5f, perc);
         colorAdjustment.saturation.value = Mathf.Lerp(0f, -30f, perc);
         lensDistortion.intensity.value = Mathf.Lerp(0f, -0.3f, perc);
 
-        bool changed = false;
-
-
-
-        #region Slomo
-        if (Input.GetButton("Slomo"))
-        {
-            Time.timeScale = Mathf.Lerp(Time.timeScale, 0.03f, 0.1f);
-            Time.fixedDeltaTime = 0.02F * Time.timeScale;
-        }
-        else
-        {
-            Time.timeScale = Mathf.Lerp(Time.timeScale, 1f, 0.12f);
-            Time.fixedDeltaTime = 0.02F * Time.timeScale;
-        }
-        #endregion
+        bool changed = UpdateSaturationCircles();
 
         if (Input.GetButton("PreviewDimension"))
         {
@@ -109,5 +166,20 @@ public class DimensionSwapping : MonoBehaviour
         {
             UpdateAllMaterials();
         }
+
+
+        #region Slomo
+        if (Input.GetButton("Slomo"))
+        {
+            Time.timeScale = Mathf.Lerp(Time.timeScale, 0.03f, 0.1f);
+            Time.fixedDeltaTime = 0.02F * Time.timeScale;
+        }
+        else
+        {
+            Time.timeScale = Mathf.Lerp(Time.timeScale, 1f, 0.12f);
+            Time.fixedDeltaTime = 0.02F * Time.timeScale;
+        }
+        #endregion
+
     }
 }
