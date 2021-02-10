22286711e0b7fd82ab8f35f183122b30bc796331
Sam <samsalfi@yahoo.ca>
2021-02-09 23:40:28 -0400

    update maybe

--- Assets/Slomo.cs
+++ Assets/Slomo.cs
@@ -6,7 +6,7 @@ public class Slomo : MonoBehaviour
 {
     void Update()
     {
-        if (Input.GetButton("Slomo"))
+        if (Input.GetButton("Slomo") && !Input.GetButtonDown("Slomo") && !Input.GetButtonUp("Slomo"))
         {
             Time.timeScale = Mathf.Lerp(Time.timeScale, 0.03f, 0.1f);
             Time.fixedDeltaTime = 0.02F * Time.timeScale;
