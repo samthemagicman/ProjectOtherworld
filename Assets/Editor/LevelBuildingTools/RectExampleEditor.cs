3c34c8d7d7da6e6c343c46571f4708efe4378778
sam <samsalfi@yahoo.ca>
2021-04-22 21:37:59 -0300

    Added corners to custom rect tool

--- Assets/Editor/LevelBuildingTools/RectExampleEditor.cs
+++ Assets/Editor/LevelBuildingTools/RectExampleEditor.cs
@@ -67,6 +67,17 @@ public class RectUtils
         return 0;
     }
 
+    public static Vector3 GridSnap(Vector3 vector3, float gridSize=1)
+    {
+        return new Vector3(Mathf.Round(vector3.x / 1) * 1, Mathf.Round(vector3.y / 1) * 1);
+    }
+
+    public static Vector2 GridSnap(Vector2 vector3, float gridSize = 1)
+    {
+        return new Vector2(  Mathf.Round(vector3.x / 1) * 1, Mathf.Round(vector3.y / 1) * 1);
+    }
+
+
     public static Rect ResizeRect(Rect rect, Handles.CapFunction capFunc, Color capCol, Color fillCol, float capSize, float snap, GameObject gameObject)
     {
         #region Mouse Changing
@@ -111,7 +122,12 @@ public class RectUtils
                 new Vector3(rect.position.x - halfRectSize.x, rect.position.y, 0),   // Left
                 new Vector3(rect.position.x + halfRectSize.x, rect.position.y, 0),   // Right
                 new Vector3(rect.position.x, rect.position.y + halfRectSize.y, 0),   // Top
-                new Vector3(rect.position.x, rect.position.y - halfRectSize.y, 0)    // Bottom 
+                new Vector3(rect.position.x, rect.position.y - halfRectSize.y, 0),    // Bottom 
+
+                new Vector3(rect.position.x + halfRectSize.x, rect.position.y + halfRectSize.y, 0),   // Top Right
+                new Vector3(rect.position.x - halfRectSize.x, rect.position.y + halfRectSize.y, 0),   // Top Left
+                new Vector3(rect.position.x + halfRectSize.x, rect.position.y - halfRectSize.y, 0),   // Bottom Right
+                new Vector3(rect.position.x - halfRectSize.x, rect.position.y - halfRectSize.y, 0)    // Bottom Left
             };
 
         Handles.color = capCol;
@@ -124,7 +140,10 @@ public class RectUtils
         var bottomHandle = Handles.Slider(handlePoints[3], -Vector3.up, rect.width / 2, Handles.RectangleHandleCap, snap).y - handlePoints[3].y;//Handles.Slider(handlePoints[3], -Vector3.up, capSize, capFunc, snap).y - handlePoints[3].y;
         Vector2 middleHandle = Handles.Slider2D(rect.position, Vector3.back, Vector3.right, Vector3.up, capSize*2, Handles.RectangleHandleCap, snap) - new Vector3(rect.position.x, rect.position.y);
 
-        Vector2 topRightHandle = Handles.Slider2D(rect.position + halfRectSize, Vector3.back, Vector3.right, Vector3.up, capSize, Handles.CubeHandleCap, snap) - new Vector3((rect.position + halfRectSize).x, (rect.position + halfRectSize).y);
+        Vector2 topRightHandle = Handles.Slider2D(handlePoints[4], Vector3.back, Vector3.right, Vector3.up, capSize, Handles.CubeHandleCap, snap) - handlePoints[4];
+        Vector2 topLeftHandle = Handles.Slider2D(handlePoints[5], Vector3.back, Vector3.right, Vector3.up, capSize, Handles.CubeHandleCap, snap) - handlePoints[5];
+        Vector2 bottomRightHandle = Handles.Slider2D(handlePoints[6], Vector3.back, Vector3.right, Vector3.up, capSize, Handles.CubeHandleCap, snap) - handlePoints[6];
+        Vector2 bottomLeftHandle = Handles.Slider2D(handlePoints[7], Vector3.back, Vector3.right, Vector3.up, capSize, Handles.CubeHandleCap, snap) - handlePoints[7];
 
 
 
@@ -134,15 +153,40 @@ public class RectUtils
         topHandle = Mathf.Round(topHandle / 1) * 1;
         bottomHandle = Mathf.Round(bottomHandle / 1) * 1;
         middleHandle = new Vector2(Mathf.Round(middleHandle.x / 1) * 1, Mathf.Round(middleHandle.y / 1) * 1);
-        topRightHandle = new Vector2(Mathf.Round(topRightHandle.x / 1) * 1, Mathf.Round(topRightHandle.y / 1) * 1);
+
+        topRightHandle = GridSnap(topRightHandle);
+        topLeftHandle = GridSnap(topLeftHandle);
+        bottomLeftHandle = GridSnap(bottomLeftHandle);
+        bottomRightHandle = GridSnap(bottomRightHandle);
 
         newSize = new Vector2(
-            Mathf.Max(.1f, newSize.x - leftHandle + topRightHandle.x + rightHandle),
-            Mathf.Max(.1f, newSize.y + topHandle + topRightHandle.y - bottomHandle));
+            Mathf.Max(.1f, newSize.x - leftHandle
+            + topRightHandle.x
+            - topLeftHandle.x
+            + bottomRightHandle.x
+            - bottomLeftHandle.x
+
+            + rightHandle),
+            Mathf.Max(.1f, newSize.y + topHandle
+            + topRightHandle.y
+            + topLeftHandle.y
+            - bottomRightHandle.y
+            - bottomLeftHandle.y
+            - bottomHandle));
 
         newPosition = new Vector2(
-            newPosition.x + leftHandle * .5f + rightHandle * .5f + middleHandle.x + topRightHandle.x/2,
-            newPosition.y + topHandle * .5f + bottomHandle * .5f + middleHandle.y + topRightHandle.y/2);
+            newPosition.x + leftHandle * .5f + rightHandle * .5f + middleHandle.x
+            + topRightHandle.x / 2
+            + topLeftHandle.x / 2
+            + bottomRightHandle.x / 2
+            + bottomLeftHandle.x / 2,
+
+            newPosition.y + topHandle * .5f + bottomHandle * .5f + middleHandle.y
+            + topRightHandle.y / 2
+            + topLeftHandle.y / 2
+            + bottomRightHandle.y / 2
+            + bottomLeftHandle.y / 2
+            );
 
         return new Rect(newPosition.x, newPosition.y, newSize.x, newSize.y);
     }
