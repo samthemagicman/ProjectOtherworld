0e23661b028552683b40f104a2c6dba634a56cfa
WHHWII <36775534+WHHWII@users.noreply.github.com>
2021-09-18 18:01:10 -0500

    Updated Menu Navigation that works with controller
    Controller doesnt select Non-button first items

--- Assets/Scripts/noahInput/MenuNavi.cs
+++ Assets/Scripts/noahInput/MenuNavi.cs
@@ -2,58 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 //sam i dont care, you can change whatever you what about this
 public class MenuNavi : MonoBehaviour
 {
     //assigning the inputhandler
     private InputHandler inputHandler;
-    private GameObject[] allChildren;
-
+    private GameObject[] allMenus;
+    private int activeMenu = 0;
+    private bool detectedController;
     //first thing that happens
     private void Awake()
     {
         inputHandler = GetComponent<InputHandler>();
-        allChildren = new GameObject[transform.childCount];
-        findMenus();
-        switchMenu(0);
+        allMenus = new GameObject[transform.childCount];
+        FindMenus();
     }
 
-    //getting a list of the menus
-    public void findMenus()
+    //getting a list of the menus 
+    //main menu = index 0 ect.
+    public void FindMenus()
     {
         int i = 0;
 
         foreach (Transform child in transform)
         {
-            allChildren[i] = child.gameObject;
+            allMenus[i] = child.gameObject;
             i += 1;
         }
     }
 
-    public void switchMenu(int newMenu)
+    public void SwitchMenu(int newMenu)
     {
-        foreach (GameObject menu in allChildren)
-        {
-            menu.SetActive(false);
-        }
-        allChildren[newMenu].SetActive(true);
-        ControllerFirstButton(allChildren[newMenu]);
+        allMenus[activeMenu].SetActive(false);
+        allMenus[newMenu].SetActive(true);
+        SetControllerFirstButton(allMenus[newMenu]);
+        activeMenu = newMenu;
     }
-
+    
+    //Check for controller each frame.
     void Update()
     {
-
+        if(inputHandler.isController && !detectedController)
+        {
+            SetControllerFirstButton(allMenus[activeMenu]);
+            detectedController = true;
+        }
+        else if (inputHandler.isKeyboard)
+        {
+            detectedController = false;
+        }
     }
+
     //if controller is being used, set button to selected.
-    public void ControllerFirstButton(GameObject menu)
+    public void SetControllerFirstButton(GameObject menu)
     {
         if(inputHandler.isController == true)
         {
             GameObject layout = menu.transform.Find("layoutGroup").gameObject;
-            EventSystem.current.SetSelectedGameObject(layout.transform.GetChild(0).gameObject);
-        } else if(inputHandler.isKeyboard == true)
-        {
-            
-        }
+            GameObject firstItem = layout.transform.GetChild(0).gameObject;
+            if (firstItem.GetComponent<Button>())
+            {
+                EventSystem.current.SetSelectedGameObject(firstItem);
+            }
+            else
+            {
+                //need something here to select non button options.
+            }
+
+
+        } 
     }
 }
