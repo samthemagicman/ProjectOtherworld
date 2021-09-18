80cf294a1df4d14364ad72a03ae390a99d537c8d
ZTattempt <actualzaustyke@github.com>
2021-09-18 16:48:17 -0500

    scripting changes
    asdasd

--- Assets/Scripts/noahInput/MenuNavi.cs
+++ Assets/Scripts/noahInput/MenuNavi.cs
@@ -5,18 +5,23 @@ using UnityEngine.EventSystems;
 //sam i dont care, you can change whatever you what about this
 public class MenuNavi : MonoBehaviour
 {
-    public InputHandler inputHandler;
-    
+    //assigning the inputhandler
+    private InputHandler inputHandler;
+    private GameObject[] allChildren;
+
+    //first thing that happens
     private void Awake()
     {
         inputHandler = GetComponent<InputHandler>();
+        allChildren = new GameObject[transform.childCount];
+        findMenus();
+        switchMenu(0);
     }
-    //first, we need to get the current menu
+
+    //getting a list of the menus
     public void findMenus()
     {
         int i = 0;
-
-        GameObject[] allChildren = new GameObject[transform.childCount];
 
         foreach (Transform child in transform)
         {
@@ -25,16 +30,30 @@ public class MenuNavi : MonoBehaviour
         }
     }
 
-    public void SelectIfController(GameObject button)
+    public void switchMenu(int newMenu)
+    {
+        foreach (GameObject menu in allChildren)
+        {
+            menu.SetActive(false);
+        }
+        allChildren[newMenu].SetActive(true);
+        ControllerFirstButton(allChildren[newMenu]);
+    }
+
+    void Update()
+    {
+
+    }
+    //if controller is being used, set button to selected.
+    public void ControllerFirstButton(GameObject menu)
     {
         if(inputHandler.isController == true)
         {
-            EventSystem.current.SetSelectedGameObject(button);
-            print(EventSystem.current.currentSelectedGameObject);
+            GameObject layout = menu.transform.Find("layoutGroup").gameObject;
+            EventSystem.current.SetSelectedGameObject(layout.transform.GetChild(0).gameObject);
         } else if(inputHandler.isKeyboard == true)
         {
-            EventSystem.current.SetSelectedGameObject(null);
-            print(EventSystem.current.currentSelectedGameObject);
+            
         }
     }
 }
